c967e926003c099d70f5c26c52aca69debdbb107
hisham waleed karam <hisham.karam@cartologic.com>
2018-06-24 02:16:33 +0200

    add Geoserver Publisher

--- handlers.py
+++ handlers.py
@@ -4,8 +4,13 @@ import pipes
 import subprocess
 from collections import namedtuple
 from sys import stdout
+import requests
 import logging
+from requests.auth import HTTPBasicAuth
+from geonode.geoserver.helpers import ogc_server_settings
+from django.conf import settings
 
+DEFAULT_WORKSPACE = settings.DEFAULT_WORKSPACE
 formatter = logging.Formatter(
     '[%(asctime)s] p%(process)s  { %(name)s %(pathname)s:%(lineno)d} \
                             %(levelname)s - %(message)s', '%m-%d %H:%M:%S')
@@ -150,3 +155,38 @@ class GpkgManager(object):
                   if layer.gpkg_layer.GetName() in layernames]
         for lyr in layers:
             ds.CopyLayer(lyr.gpkg_layer, lyr.gpkg_layer.GetName())
+
+
+class GeoserverPublisher(object):
+    def __init__(self, geoserver_url=ogc_server_settings.LOCATION,
+                 workspace=DEFAULT_WORKSPACE,
+                 datastore=ogc_server_settings.datastore_db['NAME'],
+                 geoserver_user={'username': ogc_server_settings.credentials[0],
+                                 'password': ogc_server_settings.credentials[1]}):
+        self.base_url = geoserver_url
+        self.workspace = workspace
+        self.datastore = datastore
+        self.username = geoserver_user.get(
+            'username', ogc_server_settings.credentials[0])
+        self.password = geoserver_user.get(
+            'username', ogc_server_settings.credentials[1])
+
+    @property
+    def featureTypes_url(self):
+        return "{}rest/workspaces/{}/datastores/{}/featuretypes"\
+            .format(self.base_url, self.workspace, self.datastore)
+
+    def publish_postgis_layer(self, layername):
+        if layername:
+            req = requests.post(self.featureTypes_url,
+                                headers={'Content-Type': "application/json"},
+                                auth=HTTPBasicAuth(
+                                    self.username, self.password),
+                                json={"featureType": {"name": layername,
+                                                      "nativeName": layername}
+                                      })
+            if req.status_code == 200:
+                return True
+        return False
+
+# TODO: GeonodePublisher
