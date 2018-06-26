cbfc825c05a75e3940462668208fd623ebb4bf9e
hisham waleed karam <hisham.karam@cartologic.com>
2018-06-26 12:19:33 +0200

    add get_sld_body in utils module

--- utils.py
+++ utils.py
@@ -2,8 +2,17 @@ from django.conf import settings
 from .handlers import GpkgManager
 from geonode.geoserver.helpers import (
     gs_catalog, get_store, ogc_server_settings)
+import requests
+from requests.auth import HTTPBasicAuth
 DEFAULT_WORKSPACE = settings.DEFAULT_WORKSPACE
 
+
+def get_sld_body(url):
+    req = requests.get(url,
+                       auth=HTTPBasicAuth(
+                           ogc_server_settings.credentials[0],
+                           ogc_server_settings.credentials[1]))
+    return req.text
 
 def get_connection():
     storename = ogc_server_settings.datastore_db['NAME']
