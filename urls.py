68211312e5194c6331d6baa8e0c099051d79b241
hisham waleed karam <hisham.karam@cartologic.com>
2018-07-01 06:55:15 +0200

    add endpoints urls

--- urls.py
+++ urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls import patterns, url
-from .views import publish_layer, UploadView
+from .views import (publish_layer, UploadView,
+                    deleteUpload, compare_to_geonode_layer,
+                    get_compatible_layers, reload_layer)
 urlpatterns = patterns('',
-                       url(r'^upload/$', UploadView.as_view(),
+                       url(r'^upload/', UploadView.as_view(),
                            name="geopackage_upload"),
                        url(r'^publish/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)$', publish_layer,
                            name="geopackage_publish"),
+                       url(r'^compare_schema/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<glayername>[^/]*)$', compare_to_geonode_layer,
+                           name="compare_schema"),
+                       url(r'^reload_layer/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<glayername>[^/]*)$', reload_layer,
+                           name="reload_layer"),
+                       url(r'^compatible_layers/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/$', get_compatible_layers,
+                           name="compatible_layers"),
+                       url(r'^delete/(?P<upload_id>[\d]+)/$', deleteUpload,
+                           name="geopackage_delete"),
                        )
