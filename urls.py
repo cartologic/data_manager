b80446e079fd6c08a96bc37c7918f0e83f1af0d1
hisham waleed karam <hisham.karam@cartologic.com>
2018-07-03 10:38:27 +0200

    fix django 1.11 compatibility

--- urls.py
+++ urls.py
@@ -1,22 +1,24 @@
-from django.conf.urls import patterns, url
-from .views import (publish_layer, UploadView,
-                    deleteUpload, compare_to_geonode_layer,
-                    get_compatible_layers, reload_layer, download_layers)
-urlpatterns = patterns('',
-                       url(r'^upload/', UploadView.as_view(),
-                           name="geopackage_upload"),
-                       url(r'^publish/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)$', publish_layer,
-                           name="geopackage_publish"),
-                       url(r'^publish/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<publish_name>[^/]*)$', publish_layer,
-                           name="geopackage_publish_name"),
-                       url(r'^compare_schema/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<glayername>[^/]*)$', compare_to_geonode_layer,
-                           name="compare_schema"),
-                       url(r'^reload_layer/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<glayername>[^/]*)$', reload_layer,
-                           name="reload_layer"),
-                       url(r'^compatible_layers/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/$', get_compatible_layers,
-                           name="compatible_layers"),
-                       url(r'^delete/(?P<upload_id>[\d]+)/$', deleteUpload,
-                           name="geopackage_delete"),
-                       url(r'^download$', download_layers,
-                           name="geopackage_download"),
-                       )
+from django.conf.urls import url
+
+from .views import (UploadView, compare_to_geonode_layer, deleteUpload,
+                    download_layers, get_compatible_layers, publish_layer,
+                    reload_layer)
+
+urlpatterns = [
+    url(r'^upload/', UploadView.as_view(),
+        name="geopackage_upload"),
+    url(r'^publish/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)$', publish_layer,
+        name="geopackage_publish"),
+    url(r'^publish/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<publish_name>[^/]*)$', publish_layer,
+        name="geopackage_publish_name"),
+    url(r'^compare_schema/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<glayername>[^/]*)$', compare_to_geonode_layer,
+        name="compare_schema"),
+    url(r'^reload_layer/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/(?P<glayername>[^/]*)$', reload_layer,
+        name="reload_layer"),
+    url(r'^compatible_layers/(?P<upload_id>[\d]+)/(?P<layername>[^/]*)/$', get_compatible_layers,
+        name="compatible_layers"),
+    url(r'^delete/(?P<upload_id>[\d]+)/$', deleteUpload,
+        name="geopackage_delete"),
+    url(r'^download$', download_layers,
+        name="geopackage_download"),
+]
